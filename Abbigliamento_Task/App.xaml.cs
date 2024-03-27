e721fa65a861827c17463c71d286a92e0943c1fe
Andreas Ubertini <ubertiniandreas@gmail.com>
2024-03-27 09:19:27 +0100

    Aggiungere i file di progetto.

---
+++ Abbigliamento_Task/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Abbigliamento_Task
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
